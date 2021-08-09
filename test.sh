46d5c54f805d3f0dbffbb07a678eaadbd8e62e5f
Torsten Grote <t@grobox.de>
2021-08-09 13:30:03 +0200

    Add animalSniffer to test (can fail with Java 11)

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ set -x
 
 git clone --depth 1 https://code.briarproject.org/akwizgran/briar.git briar
 cd briar
-./gradlew --no-daemon test
+./gradlew --no-daemon animalSnifferMain animalSnifferTest test
